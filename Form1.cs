4ca4267b83d191fa003d9cf5f633b192f8b5bb95
diverc <diverc@localhost>
2022-02-16 21:08:46 +0900

    context menu disable

--- Form1.cs
+++ Form1.cs
@@ -16,16 +16,19 @@ namespace WebView2Browser
         public Form1(string uri, bool viewUrl)
         {
             InitializeComponent(uri,viewUrl);
-            this.Resize += new System.EventHandler(this.Form_Resize);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            webView.NavigationStarting  += WebView_NavigationStarting;
+           // webView.NavigationCompleted += WebView_NavigationComplete;
         }
 
-        private void Form_Resize(object sender, EventArgs e)
+        private void Form1_Resize(object sender, EventArgs e)
         {
             webView.Size = this.ClientSize - new System.Drawing.Size(webView.Location);
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            this.WindowState = FormWindowState.Maximized;
 
         }
 
@@ -33,5 +36,33 @@ namespace WebView2Browser
         {
 
         }
+
+        private void WebView_NavigationStarting(object sender, CoreWebView2NavigationStartingEventArgs e)
+        {
+            if(webView.CoreWebView2 != null)
+            {
+                webView.CoreWebView2.Settings.AreDefaultContextMenusEnabled = false;
+                webView.CoreWebView2.Settings.AreDevToolsEnabled = false;
+                //MessageBox.Show("CoreWebView2 Suceess");
+            }else{
+                MessageBox.Show("Starting CoreWebView2 is null");
+            }
+
+        }
+
+/*
+        private void WebView_NavigationComplete(object sender, CoreWebView2NavigationCompletedEventArgs e)
+        {
+            if(webView.CoreWebView2 != null)
+            {
+                webView.CoreWebView2.Settings.AreDefaultContextMenusEnabled = false;
+                webView.CoreWebView2.Settings.AreDevToolsEnabled = false;
+                //MessageBox.Show("CoreWebView2 Suceess");
+            }else{
+                MessageBox.Show("Comp CoreWebView2 is null");
+            }
+
+        }
+*/
     }
 }
